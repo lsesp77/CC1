43f2fd680d1ea283b0815d7fc9752c4c619a07bf
unknown <you@example.com>
2017-04-27 14:29:06 -0500

    Practica1.2

---
+++ Practica1.2/EJ12/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+void reversa(int arreglo[],int n)
+{
+    n--;
+    int temp,i=n/2,x=0;
+    while (n>i)
+    {
+        /*
+         0  5
+         1  4
+         2  3
+        */
+        temp=arreglo[x];
+        arreglo[x]=arreglo[n];
+        arreglo[n]=temp;
+        x++;
+        n--;
+    }
+}
+int main()
+{
+    int arreglo[]={1,2,3,4,5,6},n=6;
+    reversa(arreglo,n);
+    for(int x=0;x<n;x++)
+        cout<<arreglo[x]<<"  ";
+
+    return 0;
+}
