6b4d9c590ef77c81d533b4bbb59a86bed8901a16
ALUMNO-A <ALUMNO-A@UCSP.AULAB>
2017-06-06 10:21:40 -0500

    PointArray

---
+++ PointArray17/include/Geometry.h
@@ -0,0 +1,21 @@
+#include "Point.h"
+
+class PointArray {
+        int size ;
+        Point * points ;
+        void resize (int size );
+
+    public :
+        PointArray ();
+        PointArray ( const Point pts [], const int size );
+        PointArray ( const PointArray &pv);
+        ~ PointArray ();
+
+        void clear ();
+        int getSize () const { return size ;}
+        void push_back ( const Point &p);
+        void insert ( const int pos , const Point &p);
+        void remove ( const int pos );
+        Point *get( const int pos);
+        const Point *get( const int pos) const ;
+};
---
+++ PointArray17/src/Point.cpp
@@ -0,0 +1,27 @@
+#include "Point.h"
+
+Point::Point(int _x,int _y)
+    : x(_x), y(_y) {}
+
+Point::~Point()
+{}
+
+int Point::getX()
+{
+    return x;
+}
+
+int Point::getY()
+{
+    return y;
+}
+
+void Point::setX(int _x)
+{
+    x=_x;
+}
+
+void Point::setY(int _y)
+{
+    y=_y;
+}
